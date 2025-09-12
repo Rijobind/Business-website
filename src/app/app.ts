0cf6af8d92dd0d98a3bd29a0d99d6e2ff0bbbe0e
rijobin <rijobind@mail.com>
2025-09-12 14:15:01 +0530

    Rijobin commit on next day

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('bussiness-website');
+  protected readonly title = signal('business-website');
 }
--- src/app/shared-components/header/header.ts
+++ src/app/shared-components/header/header.ts
@@ -1,44 +1,3 @@
-// import { CommonModule } from '@angular/common';
-// import { AfterViewInit, Component, HostListener } from '@angular/core';
-// import { Router } from '@angular/router';
-
-// @Component({
-//   selector: 'app-header',
-//   imports: [CommonModule],
-//   templateUrl: './header.html',
-//   styleUrl: './header.css'
-// })
-// export class Header {
-//   isScrolled = false;
-
-//   constructor(private router: Router) { }
-
-
-//   @HostListener('window:scroll', [])
-//   onWindowScroll() {
-//     this.isScrolled = window.scrollY > 50;
-//   }
-
-//   onContact() {
-//     this.router.navigate(['/contact'])
-//   }
-//   onReturnsPolicy() {
-//     this.router.navigate(['/returns-policy'])
-//   }
-//   onAcceptableUsePolicy() {
-//     this.router.navigate(['/acceptable-use-policy'])
-//   }
-//   onCookiePolicy() {
-//     this.router.navigate(['/cookie-policy'])
-//   }
-//   onTermsOfService() {
-//     this.router.navigate(['/terms-of-service'])
-//   }
-//   onPrivacyPolicy() {
-//     this.router.navigate(['/privacy-policy'])
-//   }
-// }
-
 import { CommonModule } from '@angular/common';
 import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
@@ -55,7 +14,7 @@ export class Header {
   isHomePage = false;
 
   constructor(private router: Router) {
-    this.isHomePage = this.router.url === '/'; // detect if current page is home
+    this.isHomePage = this.router.url === '/';
   }
 
   @HostListener('window:scroll', [])
@@ -63,10 +22,22 @@ export class Header {
     this.isScrolled = window.scrollY > 50;
   }
 
-  onContact() { this.router.navigate(['/contact']); }
-  onReturnsPolicy() { this.router.navigate(['/returns-policy']); }
-  onAcceptableUsePolicy() { this.router.navigate(['/acceptable-use-policy']); }
-  onCookiePolicy() { this.router.navigate(['/cookie-policy']); }
-  onTermsOfService() { this.router.navigate(['/terms-of-service']); }
-  onPrivacyPolicy() { this.router.navigate(['/privacy-policy']); }
+  onContact() {
+    this.router.navigate(['/contact']);
+  }
+  onReturnsPolicy() {
+    this.router.navigate(['/returns-policy']);
+  }
+  onAcceptableUsePolicy() {
+    this.router.navigate(['/acceptable-use-policy']);
+  }
+  onCookiePolicy() {
+    this.router.navigate(['/cookie-policy']);
+  }
+  onTermsOfService() {
+    this.router.navigate(['/terms-of-service']);
+  }
+  onPrivacyPolicy() {
+    this.router.navigate(['/privacy-policy']);
+  }
 }
